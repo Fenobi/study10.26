fa08afd08e8c135cef8de5ae06fad1cb3ff08b8e
Fenobi <13667739122@qq.com>
2022-10-26 22:20:55 +0800

    添加项目文件。

---
+++ test.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+//#include <stdio.h>
+//#include <windows.h>
+//int main()
+//{
+//	 int a = 0;
+//	 //int i=0;
+//	 printf("#\r");
+//	 while (101 - a)
+//	 {
+//		 printf("%20d%%\r", a);
+//		 fflush(stdout);
+//		 if (a > 45)
+//		 {
+//			 printf("###\r");
+//			 Sleep(10);
+//		 }
+//		 if (a > 80)
+//		 {
+//			 printf("######\r");
+//			 Sleep(50);
+//		 }
+//		 if (a > 97)
+//		 {
+//			 printf("#########\r");
+//			 Sleep(1000);
+//		 }
+//		 a++;
+//		 Sleep(20);
+//
+//	 }
+//	 printf("###########\r");
+//
+//	 return 0;
+// }
+
+
